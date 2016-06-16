82ec6efcf30c7efa80474088994c2156c094f7ac
r2h2@kalypso <r2h2.kalypso@mail.hoerbe.at>
2016-06-16 22:40:12 +0200

    add docker pull

--- startapp.sh
+++ startapp.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-docker run -i --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix rhoerbe/pvzd-client-app
+docker_image='rhoerbe/pvzd-client-app'
+
+logger -p local0.info "pulling docker image $docker_image"
+docker pull $docker_image
+logger -p local0.info "starting docker image $docker_image"
+docker run -i --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix $docker_image
