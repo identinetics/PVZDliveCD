d2f15284abe68df925d782e9938e7270d94e1e65
r2h2@kalypso <r2h2.kalypso@mail.hoerbe.at>
2016-06-17 22:34:54 +0200

    fixed startup

--- install/scripts/startapp.sh
+++ install/scripts/startapp.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-docker_image='rhoerbe/pvzd-client-app'
+DOCKER_IMAGE='rhoerbe/pvzd-client-app'
+CONTAINERNAME='pvzd-client'
 
-logger -p local0.info "pulling docker image $docker_image"
-docker pull $docker_image
-logger -p local0.info "starting docker image $docker_image"
-docker run -i --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix $docker_image
+logger -p local0.info "pulling docker image $DOCKER_IMAGE"
+docker pull $DOCKER_IMAGE
+logger -p local0.info "starting docker image $DOCKER_IMAGE"
+docker run -it --rm \
+    --hostname=$CONTAINERNAME --name=$CONTAINERNAME \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix/:/tmp/.X11-unix \
+    $DOCKER_IMAGE
