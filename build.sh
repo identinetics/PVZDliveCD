a1b9ec8ae3f90fbff5386eb87ac13d2423c15a22
r2h2@kalypso <r2h2.kalypso@mail.hoerbe.at>
2016-06-18 20:45:45 +0200

    del prev img

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+rm livecd-centos-7-live-gnome-docker*.iso
+
 sudo livecd-creator -d -v  -c sig-core-livemedia/centos-7-live-gnome-docker.cfg \
     --cache=$PROJ_HOME/livecache/ --nocleanup | tee > build.log
