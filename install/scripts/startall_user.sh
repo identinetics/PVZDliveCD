7f4ab570d56414df2106ef971bd795b15848d8df
r2h2@kalypso <r2h2.kalypso@mail.hoerbe.at>
2016-06-20 07:53:28 +0200

    add debug candy

--- install/scripts/startall_user.sh
+++ install/scripts/startall_user.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+# format debug output if using bash -x
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 zenity --notification --text "Looking for Docker data medium" --timeout 3
 logger -p local0.info "Looking for Docker data medium"
-dbus-lauch /bin/gnome-terminal --hide-menubar -e /usr/local/bin/dockerapp_mon.sh
 
 logger -p local0.info "waiting for auto-mounting of block devices to complete"
 sleep 5
@@ -16,7 +18,7 @@ done
 if [ $? -eq 0 ]
 then 
   zenity --notification --text "Docker data medium found, starting docker image" --timeout 3
-  /tmp/startapp_inv.sh
+  /tmp/startapp_inv.sh > /tmp/startapp_inv.log 2>&1
 else
   zenity --notification --text "Docker data medium not found. Connect medium and run 'sudo /usr/local/bin/predocker.sh -d /dev/<my-docker-drive>'" --timeout 3
 fi
