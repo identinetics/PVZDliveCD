d5ee2e53eb845e37b7bb428061de516ea5cac1f7
r2h2@kalypso <r2h2.kalypso@mail.hoerbe.at>
2016-06-21 13:28:12 +0200

    refactored script name

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -7,7 +7,7 @@ zenity --notification --text "Looking for Docker data medium" --timeout 3
 logger -p local0.info "Looking for Docker data medium"
 
 logger -p local0.info "waiting for auto-mounting of block devices to complete"
-sleep 5
+sleep 3
 for i in {1..10}; do
     sudo /usr/local/bin/predocker.sh >> /tmp/predocker.log 2>&1
     sleep 3
@@ -20,6 +20,6 @@ then
   zenity --notification --text "Docker data medium found, starting docker image" --timeout 3
   /tmp/startapp_inv.sh > /tmp/startapp_inv.log 2>&1
 else
-  zenity --notification --text "Docker data medium not found. Connect medium and run 'sudo /usr/local/bin/predocker.sh -d /dev/<my-docker-drive>'" --timeout 3
+  zenity --notification --text "Docker data medium not found. Connect medium and run 'sudo /usr/local/bin/start.sh -d /dev/<my-docker-drive>'" --timeout 3
 fi
 
