af19c23576db94aca39a90bcf63a0f9342e1afd4
r2h2@kalypso <r2h2.kalypso@mail.hoerbe.at>
2016-06-21 22:42:52 +0200

    better notification

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -5,12 +5,14 @@ export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 notify-send "Looking for Docker data medium"
 logger -p local0.info "Looking for Docker data medium"
-
+sleep 2
+notify-send "waiting for auto-mounting of block devices to complete"
 logger -p local0.info "waiting for auto-mounting of block devices to complete"
-sleep 3
+sleep 2
 for i in {1..10}; do
     sudo /usr/local/bin/predocker.sh >> /tmp/predocker.log 2>&1
-    sleep 3
+    zenity --error --text "Docker data medium not found - retrying in 5 s"
+    sleep 5
     [ $? -eq 0 ] && break
 done
 
@@ -20,6 +22,6 @@ then
   notify-send  "Docker data medium found, starting docker image"
   /tmp/startapp_inv.sh > /tmp/startapp_inv.log 2>&1
 else
-  notify-send "Docker data medium not found. Connect medium and run 'sudo /usr/local/bin/start.sh -d /dev/<my-docker-drive>'" --timeout 3
+  notify-send "Docker data medium not found. Connect a marked medium (see doc) and run 'sudo /usr/local/bin/start.sh -d /dev/<my-docker-drive>'" --timeout 3
 fi
 
