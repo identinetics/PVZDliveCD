d263e827f5f864e4e8a013901c2413c15fc79349
r2h2@kalypso <r2h2.kalypso@mail.hoerbe.at>
2016-06-26 10:40:23 +0200

    fix privileges when starting script as liveuser

--- install/scripts/startapp.sh
+++ install/scripts/startapp.sh
@@ -22,25 +22,30 @@ while getopts ":hpt" opt; do
 done
 shift $((OPTIND-1))
 
+if [ $(id -u) -ne 0 ]; then
+    sudo="sudo"
+fi
+
 notify-send "Pulling docker image $DOCKER_IMAGE; please wait"  -t 50000
 logger -p local0.info "pulling docker image $DOCKER_IMAGE"
-docker pull $DOCKER_IMAGE
+$sudo docker pull $DOCKER_IMAGE
 notify-send "Docker image $DOCKER_IMAGE up-to date; starting container"
 
 DOCKERDATA_DIR=$(cat /tmp/dockerdata_dir)
 logger -p local0.info "mapping container user's home to $DOCKERDATA_DIR"
-mkdir -p $DOCKERDATA_DIR/home/liveuser/
-chown -R liveuser:liveuser $DOCKERDATA_DIR/home/liveuser/
+$sudo mkdir -p $DOCKERDATA_DIR/home/liveuser/
+$sudo chown -R liveuser:liveuser $DOCKERDATA_DIR/home/liveuser/
 
 CONTAINERNAME='x11-app'
 
 # remove dangling container
-if [ "$(docker ps -a | grep $CONTAINERNAME)" == "$CONTAINERNAME" ]; then
-    docker rm $(docker ps -a -q)
+if $sudo docker ps -a | grep $CONTAINERNAME > /dev/null; then
+    logger -p local0.info "deleting dangling container $CONTAINERNAME"
+    $sudo docker rm $CONTAINERNAME
 fi
 
 logger -p local0.info "starting docker image $DOCKER_IMAGE"
-docker run $runopt --rm \
+$sudo docker run $runopt --rm \
     --hostname=$CONTAINERNAME --name=$CONTAINERNAME \
     --log-driver=syslog --log-opt syslog-facility=local0 \
     --privileged -v /dev/bus/usb:/dev/bus/usb \
